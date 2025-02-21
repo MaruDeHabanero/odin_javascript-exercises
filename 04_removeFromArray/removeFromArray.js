173655a6eb4e7dec31d9abfb62ef77bc326f1fa8
marudehabanero <jose13.maciasm@gmail.com>
2025-02-21 14:32:39 -0600

    removeFromArray done
    I lost like 45 minutes trying to understand why 'arg' was 0

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(list, ...args) {
+	// First attempt: "const arg in args" returns the index, not the value. You dumb fuck.
+	for (const arg of args){
+		for (let i = 0; i < list.length; i++){
+			if (arg === list[i]){
+				list.splice(i, 1);
+				// If found, check again if the same index has also the same number.
+				// Keep deleting until no more matches of array is found.
+				i--;
+			}
+		}
+	}
+	return list;
 };
 
 // Do not edit below this line
