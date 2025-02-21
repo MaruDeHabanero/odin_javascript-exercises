d8479faebe568d34981d973964274c0613dde239
marudehabanero <jose13.maciasm@gmail.com>
2025-02-21 13:41:18 -0600

    reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(message) {
+	let reverse = "";
+	for (let i = message.length - 1; i >= 0; i--){
+		reverse += message[i];
+	}
+	return reverse;
 };
 
 // Do not edit below this line
