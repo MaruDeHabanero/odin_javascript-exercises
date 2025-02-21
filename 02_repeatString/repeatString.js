da9879f540b899fc3238c0931a9aa9d67437abb9
marudehabanero <jose13.maciasm@gmail.com>
2025-02-21 13:29:45 -0600

    repeatString done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = (word, number) => {
+	if (number < 0) return "ERROR";
+	let message = "";
+	for (let x = 0; x < number; x++){
+		message += word;
+	}
+	return message;
 };
 
 // Do not edit below this line
