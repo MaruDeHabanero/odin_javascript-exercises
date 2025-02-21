2b8f9a4a9274f09fd31af8881bbc4ce6c00d5373
marudehabanero <jose13.maciasm@gmail.com>
2025-02-21 14:55:24 -0600

    sumAll done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+	if (isNaN(x) || isNaN(y)) return 'ERROR';
+	if (!Number.isInteger(x) || !Number.isInteger(y)) return 'ERROR';
+	if (x < 0 || y < 0) return 'ERROR';
+	
+	let small = x;
+	let big = y;
 
+	if (x > y){
+		small = y;
+		big = x;
+	}
+
+	let sum = 0;
+	for (let i = small; i <= big; i++){
+		sum += i;
+	}
+
+	return sum;
 };
 
 // Do not edit below this line
