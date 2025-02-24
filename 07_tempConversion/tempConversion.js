ec5ef4068684f48eaeff96720f3ec6d52c5ac83c
marudehabanero <jose13.maciasm@gmail.com>
2025-02-23 21:46:20 -0600

    tempConversion done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+// The functions are one liners since the formulas are found in Wikipedia.
+
+const convertToCelsius = function(tempInF) {
+	let tempInCelcius = (tempInF - 32) * (5/9);
+	return Math.round(tempInCelcius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+	let tempInFahrenheit = (tempInC * 9/5 + 32);
+	return Math.round(tempInFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
