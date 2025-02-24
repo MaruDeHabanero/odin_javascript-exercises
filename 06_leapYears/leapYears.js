67a126846cc1808cd9018cbb54f624571eddc697
marudehabanero <jose13.maciasm@gmail.com>
2025-02-23 21:35:37 -0600

    leapYears done

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+	if (year % 4 != 0) return false;
+	if (year % 100 == 0){
+		return year % 400 == 0;
+	} else {
+		return true;
+	}
 };
 
+// TODO: aquí te quedaste, salieron fail en los tests (sdxxdxdxd)
 // Do not edit below this line
 module.exports = leapYears;
