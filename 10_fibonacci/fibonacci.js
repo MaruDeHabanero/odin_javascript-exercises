02fb6dd6a7d7da508d11f2b3dba690e1984959eb
marudehabanero <jose13.maciasm@gmail.com>
2025-02-25 17:52:55 -0600

    Fibonacci done
    not gonna lie, really proud on this one since I didn't look at any solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+	index = Number(index);
+	if (index < 0) return "OOPS";
+	if (index == 0) return 0;
 
+	let last = 0;
+	let present = 1;
+	let sum = 0;
+
+	for (let i = 0; i < index; i++){
+		sum = last + present;
+		last = present;
+		present = sum;
+	}
+
+	return last;
 };
 
 // Do not edit below this line
