9fe2735d7077cad3e53deafc02a03febb6844095
marudehabanero <jose13.maciasm@gmail.com>
2025-02-25 16:59:06 -0600

    calculator done
    I got really confused on the power one.
    I'm kinda stupid but I got it done lol.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+	array.forEach((item) => sum += item);
+	return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let mult = 1;
+	array.forEach((item) => mult *= item);
+	return mult;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	const constant = a;
+	for (let i = 1; i < b; i++){
+		a *= constant;
+	}
+	return a;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num == 0) return 1;
+	if (num <= 2) return num;
+
+	for (let i = num - 1; i > 1; i--){
+		num *= i;
+	}
+
+	return num;
 };
 
 // Do not edit below this line
