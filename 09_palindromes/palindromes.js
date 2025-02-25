49fad70740d61514375770fa44f0301846d15352
marudehabanero <jose13.maciasm@gmail.com>
2025-02-25 17:28:47 -0600

    Palindrome done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (word) {
+	word = word.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").replaceAll(" ","");
 
+	for (let i = 0; i < word.length; i++){
+		if (word[i] != word[word.length - i - 1]){
+			return false;
+		}
+	}
+	
+	return true;
 };
 
 // Do not edit below this line
