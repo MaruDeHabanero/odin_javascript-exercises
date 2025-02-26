9384733df1b45359878abdbc90fe77b21cc63f70
marudehabanero <jose13.maciasm@gmail.com>
2025-02-26 17:06:32 -0600

    findTheOldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+function getAge(yearBirth, yearDead){
+	return yearDead - yearBirth;
+}
 
+const findTheOldest = function(people) {
+	let oldestPerson = "";
+
+	const currentYear = 2025;
+	let oldestAge = 0;
+
+	for (person of people){
+		let age = 0;
+		
+		if (person.yearOfDeath === undefined){
+			age = getAge(person.yearOfBirth, currentYear);
+		} else {
+			age = getAge(person.yearOfBirth, person.yearOfDeath);
+		}
+
+		if (age > oldestAge){
+			oldestAge = age;
+			oldestPerson = person;
+		}
+	}
+
+	return oldestPerson;
 };
 
 // Do not edit below this line
