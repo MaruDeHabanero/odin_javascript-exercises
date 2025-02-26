10cef4680c146e583c6b389353b4d4235776232e
marudehabanero <jose13.maciasm@gmail.com>
2025-02-26 16:32:07 -0600

    getTheTitles done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+	// Looked this one over in class.
+	// Map returns the object referenced in the map function. 
+	// So, since each element of the array is an object, the argument becomes the object itself.
+	// And object.title just returns the title of each object
+	// This comment is so unnecesary but I need to write stuff to understand it so don't mind me.
+	// hel yea
+	return titles = array.map((object) => object.title);
 };
 
 // Do not edit below this line
